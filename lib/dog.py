a0eb66406480a8d78acbdc819e476682e95a5200
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-08-29 21:40:47 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,12 @@
 
 class Dog:
     pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name       # Assign name to self.name
+        self.breed = breed     # Assign breed to self.breed, defaults to "Mutt"
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
